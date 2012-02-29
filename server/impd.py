1713cb2199b2a29fc64a2db09a8d0e4fecce76fc
Jacob Hinkle <jacob@sci.utah.edu>
2012-02-29 14:13:25 -0700

    Playing with python's unix socket interface

--- server/impd.py
+++ server/impd.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env python
 
 import argparse
-import http.server
+import socket, sys, os
 
-class impHandler(http.server.BaseHTTPRequestHandler):
-    """
-    The basic request handler for imp
-    """
-    
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("I heard your GET request but I'm a dumb imp and don't know what to do with it","UTF-8"))
-        return
+def run(socket_address='impsocket', backlog=5, size=1024):
+    # Make sure the socket does not already exist
+    try:
+        os.unlink(socket_address)
+    except OSError:
+        if os.path.exists(socket_address):
+            raise
 
-def run(server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler, port=8000):
-    server_address = ('', port)
-
-    print('Starting server on port {port}'.format(port=port))
+    print('Starting server at {addr}'.format(addr=socket_address))
     print('Press Ctrl+C to stop')
 
-    httpd = server_class(server_address, handler_class)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        # Clean up stuff before exiting
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(socket_address)
 
-        print('Shutting down...')
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print('waiting for a connection',file=sys.stderr)
+        connection, client_address = sock.accept()
+        try:
+            print('connection from {client}'.format(client=client_address),file=sys.stderr)
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(100)
+                print('received {data}'.format(data=data),file=sys.stderr)
+                if data:
+                    print('sending data back to the client',file=sys.stderr)
+                    connection.sendall(data)
+                else:
+                    print('no more data from {client}'.format(client=client_address),file=sys.stderr)
+                    break
+                
+        finally:
+            # Clean up the connection
+            connection.close()
 
 if __name__ == '__main__':
     # parse command line args
     parser = argparse.ArgumentParser(description='IMP: a basic scientific data server')
-    parser.add_argument('--port', metavar='p', type=int, help='Port on which to listen', default=8000)
+    #parser.add_argument('--port', metavar='p', type=int, help='Port on which to listen', default=8000)
+    parser.add_argument('--socket', metavar='s', type=str, help='Address of socket file', default='impsocket')
 
     args = parser.parse_args()
 
     # set up internal data structures...
 
+
     # do the damn thing
-    run(handler_class=impHandler, port=args.port)
+    run(socket_address=args.socket)
