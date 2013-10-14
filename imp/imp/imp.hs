798b61fd9b41670054e41481edd4bb85d774a88b
Jacob Hinkle <jacob@sci.utah.edu>
2013-10-13 22:55:44 -0600

    More messing with types, started to alter talk for our purposes

--- imp/imp/imp.hs
+++ imp/imp/imp.hs
@@ -13,7 +13,7 @@ import System.IO
 import Data.Time.Clock
 import Text.Printf
 
--- Our own class for allowed primitive types
+-- Our own class for allowed primitive element types
 class Primitive
 instance Primitive Float
 instance Primitive Double
@@ -31,10 +31,22 @@ data Share = Share { mTime :: UTCTime, payload :: ShareArray }
 -- publisher account. holds info about the publisher, and a list of their shares
 data PublisherAcct = PublisherAcct { shares :: Map.Map String Share }
 
+-- this holds info about currently connected clients who are watching
+-- published shares for updates.  watched shares will send push
+-- notifications to these clients whenever a share is updated.
+-- TODO: This is not the right data structure for this.  Instead, we need a
+-- structure that we links Shares to Clients but is also cleaned up easily
+-- when a client disconnects
+data SubscriberAcct = SubscriberAcct { subbedShares :: [Share] }
+
+-- this can hold info about connected clients
+data Client
+
 -- server just holds a bunch of accounts, indexed by user name
 -- TODO: I think we'll put each level inside a tvar for more granular
 -- atomicity-JDH
-data Server = Server { publishers :: TVar (Map.Map String PublisherAcct) }
+data Server = Server { publishers :: TVar (Map.Map String PublisherAcct)
+                    , subscribers :: TVar (Map.Map String SubscriberAcct) }
 
 -- creating a new TVar requires us to be in IO, hence this helper fn
 newServer :: IO Server
@@ -67,3 +79,20 @@ forkFinally action and_then =
 -- I think this is a more general pattern that exists already, like we
 -- could use forkIOWithUnmask or bracket or something, but I am unfamiliar
 -- here so I'm just blindly using this until I understand it better -JDH
+
+-- Straight outta Marlow's slides.
+talk :: Server -> Handle -> IO ()
+talk server@Server{..} handle = do
+    hSetNewlineMode handle universalNewlineMode
+    hSetBuffering handle LineBuffering
+    readName
+  where
+    readName = do
+        m <- checkAddClient server name handle
+        case m of
+            Nothing -> do
+                hPrintf handle "The name %s is in use“ name
+                readName
+            Just client -> do
+                runClient server client 
+                    `finally` removeClient server name
